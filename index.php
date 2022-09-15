2b5275f424e5c8c211283f74f7aecc5a63c1fd95
di-Timonix <97761906+di-Timonix@users.noreply.github.com>
2022-09-15 19:17:10 +0200

    add atom class to index
    Signed-off-by: di-Timonix <97761906+di-Timonix@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-timonix | atom
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atom Framework V.0.1</title>
+</head>
+<body>
+    <h1>The Atom Framework!</h1>
+    <br>
+
+    <?php
+    
+        $atom = new Atom();
+        var_dump($atom);
+
+    ?>
+
+</body>
+</html>

d908364c39801c31de51948e56e7cc0c265d7745
di-Timonix <97761906+di-Timonix@users.noreply.github.com>
2026-05-24 21:37:56 +0200

    improving performance and behavior home, register in Views
    Signed-off-by: di-Timonix <97761906+di-Timonix@users.noreply.github.com>

--- resources/Views/register.php
+++ resources/Views/register.php
@@ -8,17 +8,24 @@ $form = new Form();
 
 <h1>Register</h1>
 
-<?php $form = Form::begin('', 'post') ?>
+<?php $form = Form::begin('', 'post', ["class" => "form-post"]) ?>
     <div class="row">
         <div class="col">
-            <?php echo $form->field($model, 'firstname') ?>
+            <?php echo $form->field($model, 'username') ?>
         </div>
         <div class="col">
-            <?php echo $form->field($model, 'lastname') ?>
+            <?php echo $form->field($model, 'firstName') ?>
+        </div>
+        <div class="col">
+            <?php echo $form->field($model, 'lastName') ?>
         </div>
     </div>
-    <?php echo $form->field($model, 'email') ?>
-    <?php echo $form->field($model, 'password')->passwordField() ?>
-    <?php echo $form->field($model, 'passwordConfirm')->passwordField() ?>
+    <?php // echo $form->field($model, 'email', )->emailField() ?>
+    <?php // echo $form->field($model, 'password')->passwordField() ?>
+    <?php // echo $form->field($model, 'passwordConfirm')->passwordField() ?>
+    <?php // echo $form->textareaField($model, 'textarea') ?>
+    <?php // echo $form->selectField($model, 'optionone') ?>
+    <?php // echo $form->progressField($model, 'pro') ?>
+    <?php // echo $form->meterField($model, 'met') ?>
     <button class="btn btn-success">Submit</button>
 <?php Form::end() ?>
